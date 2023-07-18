db350c2ecc245734ecfb46a431e17a33dda8327e
Rasib0 <rasibnadeem101@gmail.com>
2023-07-18 15:40:31 +0500

    animation for changing theme

--- src/content/config.ts
+++ src/content/config.ts
@@ -17,7 +17,24 @@ const postsCollection = defineCollection({
     tags: z.array(z.string())
   })
 });
+
+const projectCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string()
+    }),
+    tags: z.array(z.string())
+  })
+})
+
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   posts: postsCollection,
+  projects: projectCollection,
 };
+

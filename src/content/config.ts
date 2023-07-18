2119142fd60efbdf3eed554c34c4ad6d36cb6b41
Rasib0 <rasibnadeem101@gmail.com>
2023-07-18 18:55:56 +0500

    added project dir

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,7 +15,7 @@ const postsCollection = defineCollection({
       alt: z.string()
     }),
     tags: z.array(z.string())
-  })
+  }).strict()
 });
 
 const projectCollection = defineCollection({
@@ -23,13 +23,15 @@ const projectCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     pubDate: z.date(),
+    link: z.string().url(),
+    repo: z.string().url(),
     description: z.string(),
     image: z.object({
       url: z.string(),
       alt: z.string()
     }),
     tags: z.array(z.string())
-  })
+  }).strict()
 })
 
 // Export a single `collections` object to register your collection(s)
